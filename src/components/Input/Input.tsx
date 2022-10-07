bad2fc32edc4693647c3b6dc070c23c710302609
Wraeth <wraeth.dao@gmail.com>
2022-10-08 08:43:06 +1100

    move input

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,32 @@
+import { ReactNode, ChangeEventHandler } from 'react'
+
+export const Input = ({
+  name,
+  label,
+  disabled,
+  onChange,
+}: {
+  name: string
+  label: ReactNode
+  disabled?: boolean
+  onChange?: ChangeEventHandler<HTMLInputElement>
+}) => {
+  return (
+    <div className="relative">
+      <input
+        type="text"
+        id={name}
+        className="border-1 peer block w-full appearance-none rounded-t-lg border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+        placeholder=" "
+        disabled={disabled}
+        onChange={onChange}
+      />
+      <label
+        htmlFor={name}
+        className="absolute top-2 left-1 z-10 origin-[0] -translate-y-4 scale-75 transform bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 dark:bg-gray-800 dark:text-gray-400 peer-focus:dark:text-blue-500"
+      >
+        {label}
+      </label>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,12 @@
 import type { NextPage } from 'next'
-import { ChangeEventHandler, ReactNode, useState } from 'react'
+import { ChangeEventHandler, useState } from 'react'
 import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete'
 import useOnclickOutside from 'react-cool-onclickoutside'
 import { ExternalLink } from '@components/ExternalLink'
 import { Twitter } from '@components/icons/Twitter'
 import { queryWeather, WeatherData } from 'utils/queryWeather'
 import { GitHub } from '@components/icons/GitHub'
-
-const FloatingInput = ({
-  name,
-  label,
-  disabled,
-  onChange,
-}: {
-  name: string
-  label: ReactNode
-  disabled?: boolean
-  onChange?: ChangeEventHandler<HTMLInputElement>
-}) => {
-  return (
-    <div className="relative">
-      <input
-        type="text"
-        id={name}
-        className="border-1 peer block w-full appearance-none rounded-t-lg border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
-        placeholder=" "
-        disabled={disabled}
-        onChange={onChange}
-      />
-      <label
-        htmlFor={name}
-        className="absolute top-2 left-1 z-10 origin-[0] -translate-y-4 scale-75 transform bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 dark:bg-gray-800 dark:text-gray-400 peer-focus:dark:text-blue-500"
-      >
-        {label}
-      </label>
-    </div>
-  )
-}
+import { Input } from '@components/Input'
 
 const Home: NextPage = () => {
   const [weatherData, setWeatherData] = useState<WeatherData>()
@@ -89,7 +59,7 @@ const Home: NextPage = () => {
           <h1 className="capitalize">Do I need a scarf</h1>
           <div ref={ref}>
             <div className="px-4 pt-6 sm:px-0">
-              <FloatingInput name="test" label="Address" disabled={!ready} onChange={handleInput} />
+              <Input name="address" label="Address" disabled={!ready} onChange={handleInput} />
             </div>
             {status === 'OK' && (
               <ul className="rounded-b-lg bg-gray-500 py-1">{renderSuggestions()}</ul>
